036d03577538fcc4fc5ce5970c772b71b588d678
Lenard Pratt <l3np27@gmail.com>
2019-11-10 18:59:23 +0000

    Added date=picker

--- src/components/forms/AddTodo.tsx
+++ src/components/forms/AddTodo.tsx
@@ -3,11 +3,20 @@ import { values } from "mobx"
 import { FormField, FormCheckBox, FormSelect } from "../website/Form"
 import Card from "../website/Card"
 import theme from "../../theme/theme"
-
-import { MenuItem, FormControl } from "@material-ui/core"
+import AddIcon from "@material-ui/icons/Add"
+import DoneIcon from "@material-ui/icons/Done"
+import {
+  MenuItem,
+  FormControl,
+  Button,
+  Fab,
+  FormGroup
+} from "@material-ui/core"
 import { makeStyles } from "@material-ui/core/styles"
 import { useState } from "react"
-
+import { MuiPickersUtilsProvider } from "@material-ui/pickers"
+import DateFnsUtils from "@date-io/dayjs"
+import { DatePicker } from "@material-ui/pickers"
 interface IAddTodoFormProps {
   priorities: IPriority[]
 }
@@ -20,6 +29,10 @@ interface IPriority {
 const useStyles = makeStyles((theme) => ({
   nested: {
     paddingLeft: theme.spacing(4)
+  },
+  actions: {
+    display: "flex",
+    justifyContent: "space-between"
   }
 }))
 
@@ -29,7 +42,8 @@ const AddTodoForm: React.FC<IAddTodoFormProps> = () => {
     initialValues: {
       description: "",
       completed: false,
-      priortiy: ""
+      date: new Date(),
+      priority: ""
     },
     onSubmit: (values) => {
       console.log(JSON.stringify(values, null, 2))
@@ -42,52 +56,70 @@ const AddTodoForm: React.FC<IAddTodoFormProps> = () => {
     setOpen(!open)
   }
   return (
-    <Card
-      className="pa-6 ma-4 flex"
-      justify="center"
-      padding={theme.spacing(1)}
-    >
-      <form
-        className="mb-4 pb-4 w-full max-w-sm"
-        onSubmit={formik.handleSubmit}
+    <MuiPickersUtilsProvider utils={DateFnsUtils}>
+      <Card
+        className="pa-6 ma-4 flex"
+        justify="center"
+        padding={theme.spacing(1)}
       >
-        <FormControl>
-          <FormField
-            value={formik.values.description}
-            name="description"
-            placeholder="What to do ?"
-            required
-            handleChange={formik.handleChange}
-          ></FormField>
+        <form
+          className="mb-4 pb-4 w-full max-w-sm"
+          onSubmit={formik.handleSubmit}
+        >
+          <FormControl>
+            <FormField
+              value={formik.values.description}
+              name="description"
+              placeholder="What to do ?"
+              required
+              handleChange={formik.handleChange}
+            ></FormField>
 
-          <FormSelect
-            handleChange={formik.handleChange}
-            required
-            name="priortiy"
-            helperText="How much time needed"
-            label="priortiy"
-            value={formik.values.priortiy}
-          >
-            <MenuItem value="">
-              <em>None</em>
-            </MenuItem>
-            <MenuItem value={10}>Ten</MenuItem>
-            <MenuItem value={20}>Twenty</MenuItem>
-            <MenuItem value={30}>Thirty</MenuItem>
-          </FormSelect>
+            <FormSelect
+              handleChange={formik.handleChange}
+              required
+              name="priority"
+              helperText="How much time needed"
+              label="Priority"
+              value={formik.values.priority}
+            >
+              <MenuItem value="">
+                <em>None</em>
+              </MenuItem>
+              <MenuItem value={10}>Ten</MenuItem>
+              <MenuItem value={20}>Twenty</MenuItem>
+              <MenuItem value={30}>Thirty</MenuItem>
+            </FormSelect>
 
-          <FormCheckBox
-            className="ma-2"
-            label="Completed"
-            value={formik.values.completed}
-            name="completed"
-            handleChange={formik.handleChange}
-          />
-        </FormControl>
+            <DatePicker
+              value={formik.values.date}
+              onChange={(date) => {
+                formik.setFieldValue("date", date)
+              }}
+            />
 
-        {/* <button>Completed</button> */}
-      </form>
-    </Card>
+            <FormCheckBox
+              className="ma-2"
+              label="Completed"
+              value={formik.values.completed}
+              name="completed"
+              handleChange={formik.handleChange}
+            />
+
+            <div className={classes.actions}>
+              <Fab color="primary">
+                <AddIcon />
+              </Fab>
+              <Fab onClick={() => formik.handleSubmit()}>
+                <DoneIcon />
+              </Fab>
+            </div>
+          </FormControl>
+
+          {/* <button>Completed</button> */}
+        </form>
+      </Card>
+    </MuiPickersUtilsProvider>
   )
 }
 
